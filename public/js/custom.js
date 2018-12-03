77ea1e46bec6027ecf9722025afcefc4054b560b
Marko Anastasov <marko@renderedtext.com>
2018-12-03 16:35:43 +0100

    Update custom.js

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,5 +1,5 @@
 function linkifyHeaders() {
-  $.each($("h2,h3,h4,h5"), function (index, h) {
+  $.each($("#fullArticle").find("h2,h3,h4,h5"), function (index, h) {
     var content = "<a href='#" + $(h).attr("id") + "'>" + $(h).text() + "</a>";
     $(h).html(content);
   });
